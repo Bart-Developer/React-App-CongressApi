3438a2e7bdda608af80b8563647c41eb25673cab
Bartolome Albarracin <bartolome.albarracin@hotmail.com>
2021-08-12 01:31:38 -0300

    agrego custom hook

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import { getSenators } from '../services/getSenators.jsx'
+
 
 export default function Header() {
     
-    getSenators();
     
     return (
         <>
--- src/services/getData.jsx
+++ src/services/getData.jsx
@@ -1,6 +1,8 @@
 
-export const getSenators = async () => {
+export const getData= async () => {
     
+    // const url = 'https://api.propublica.org/congress/v1/113/house/members.json' <- add
+
     const url = 'https://api.propublica.org/congress/v1/113/senate/members.json'
     const key = {
         method: 'GET',
@@ -9,7 +11,7 @@ export const getSenators = async () => {
         }
     }
     const resp = await fetch(url, key)
-    const { results:[senators] } = await resp.json();
+    const { results:[data] } = await resp.json();
     
-        return senators;
+        return data;
 }
