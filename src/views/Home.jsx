c2ff9a59e6f79f62d9e1b156ce9a55a8e9495635
DESKTOP-GE1GSCB\aguss <aguss_979@hotmail.com>
2021-08-11 23:14:33 -0300

    Main

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import Header from '../components/Header.jsx';
-import Navbar from '../components/Navbar.jsx';
+import Header from '../components/Header';
+
+import Navbar from '../components/Navbar';
 
 const Home = () => {
    
@@ -10,6 +11,11 @@ const Home = () => {
             <Header />
             <Navbar />
 
+            <main>
+                <div className="contentMain">
+                    
+                </div>
+            </main>
         </>
     )
 }
