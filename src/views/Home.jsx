a051430b4d4ce578c756d06c0c6a0fe94d428848
DESKTOP-GE1GSCB\aguss <aguss_979@hotmail.com>
2021-08-11 21:06:24 -0300

    Trying

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,6 +4,9 @@ export const home = () => {
     return (
         <div>
            <h1>Hola! soy un componente</h1> 
+           <h1>Hola! soy un componente</h1> 
+           <h1>Hola! soy un componente</h1> 
+           <h1>Hola! soy un componente</h1> 
         </div>
     )
 }
