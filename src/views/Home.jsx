6e010c46151390ec9c163c871bbb03226399ab76
Bartolome Albarracin <bartolome.albarracin@hotmail.com>
2021-08-11 21:12:21 -0300

    prueba final

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,18 +1,11 @@
 import React from 'react'
 
-export const home = () => {
+const Home = () => {
     return (
         <div>
-           <h1>Hola! soy un componente</h1> 
-           <h1>Hola! soy un componente</h1> 
-           <h1>Hola! soy un componente</h1> 
-           <h1>Hola! soy un componente</h1>
-           <h1>soy un perrito malvado!!</h1> 
-           <h1>soy un perrito malvado!!</h1> 
-           <h1>soy un perrito malvado!!</h1> 
-           <h1>soy un perrito malvado!!</h1> 
-           <h1>soy un perrito malvado!!</h1> 
-           <h1>soy un perrito malvado!!</h1> 
+           <h1>Hola! soy un componente</h1>  
         </div>
     )
 }
+
+export default Home;
