8adb6caeec979b4a80aeff7c3e4b2100cf251481
Bartolome Albarracin <bartolome.albarracin@hotmail.com>
2021-08-15 20:17:03 -0300

    agrego botones filtros

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -9,6 +9,17 @@ const Table = ({ congress }) => {
         /*ACÁ PINTAS LA TABLA ↓ */
 
         <div className="mainTable">
+            <div className="btn-group" role="group" aria-label="Basic radio toggle button group">
+                <input type="radio" className="btn-check" name="btnradio" id="btnradio1" />
+                <label className="btn btn-outline-primary" htmlFor="btnradio1">Republicans</label>
+
+                <input type="radio" className="btn-check" name="btnradio" id="btnradio2" />
+                <label className="btn btn-outline-primary" htmlFor="btnradio2">Democrats</label>
+
+                <input type="radio" className="btn-check" name="btnradio" id="btnradio3" />
+                <label className="btn btn-outline-primary" htmlFor="btnradio3">Independents</label>
+
+            </div>
             <table className="table table-dark table-hover ">
                 <thead>
                     <tr className="text-danger">
@@ -21,19 +32,19 @@ const Table = ({ congress }) => {
                 </thead>
                 <tbody>
 
-            {
+                    {
 
-                congress.map( congressman =>
-                    
-                    <tr key={congressman.id}>
-                    <td>{ congressman.name } {  congressman.last_name} </td>
-                    <td>{ congressman.party }</td>
-                    <td>{ congressman.state }</td>
-                    <td>{ congressman.years_in_office }</td>
-                    <td>{ congressman.votes_with_party_pct }</td>
-                    </tr>
-                )
-            } 
+                        congress.map(congressman =>
+
+                            <tr key={congressman.id}>
+                                <td>{congressman.name} {congressman.last_name} </td>
+                                <td>{congressman.party}</td>
+                                <td>{congressman.state}</td>
+                                <td>{congressman.years_in_office}</td>
+                                <td>{congressman.votes_with_party_pct}</td>
+                            </tr>
+                        )
+                    }
                 </tbody>
             </table>
         </div>
