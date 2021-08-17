175eb2b10d1750dd00a3c7e0e6f9e41deb2531a4
Bartolome Albarracin <bartolome.albarracin@hotmail.com>
2021-08-17 20:05:34 -0300

    agrego loyalty

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -65,6 +65,22 @@ const Navbar = ( { data } ) => {
                                     </button>
                                 </div>
                             </div>
+                            <div className="accordion-item">
+                                <h2 className="accordion-header" id="headingFour">
+                                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                        Loyalty
+                                    </button>
+                                </h2>
+                                <div id="collapseFour" className="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+                                    <button onClick={()=> {toggleComponents('Loyalty' , 'house')}} className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"  aria-expanded="false" aria-controls="collapseTwo">
+                                        House
+                                    </button>
+
+                                    <button onClick={()=> {toggleComponents('Loyalty', 'senate')}} className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"  aria-expanded="false" aria-controls="collapseTwo">
+                                        Senate
+                                    </button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
